a3d6dd6960266295fea4923b071385209adf1903
Atharva <atharvachougale.dev@gmail.com>
2025-09-24 22:03:07 +0530

    environment setup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def main():
     print("Hello from langchain-start!")
+    print(os.environ.get("GOOGLE_API_KEY"))
 
 
 if __name__ == "__main__":
